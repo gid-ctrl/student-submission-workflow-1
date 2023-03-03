682453311999543fbbc2c4e787685dc4aa52812d
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-03-03 15:21:47 +0000

    done part of the core.js

---
+++ js-fundamentals-objects/src/core.js
@@ -0,0 +1,53 @@
+// Don't change the person variable directly on line 2
+const person = { age: 32, size: 'Small' }
+
+// 1. Give the person object a name property with the value Matt
+
+person.name = 'Matt'
+console.log("1- added Matt", person);
+
+// 2. Remove the size property from the person
+
+delete person.size
+console.log("removed size", person)
+
+// 3. Increase the person's age by 11
+
+person['age'] = 43
+console.log("increase age by 11", person)
+
+// 4. Add an instruments property to the person, initialised as an empty array
+
+person[instruments], ""
+console.log("added instruments property", person)
+
+// 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
+
+person['instrument'] = 'Guitar', 'Piano', 'Vocals'
+console.log("added 3 new instruments", person)
+
+// 6. Using an index on the instruments array, set the mainInstrument variable below
+// to the third instrument in the array
+const mainInstrument = undefined
+
+
+// 7. Add a profession property to the person, which is an object
+
+// 8. Add a name property to the profession object with the value Musician
+
+// 9. Add a friends property to the person, which is an empty array
+
+// 10. Add two objects to the persons friends array with the following properties:
+// Friend one: name - Chris, age - 46
+// Friend two: name - Dom, age - 43
+
+// 11. Using an index on the persons friends array, set the bestFriend variable below
+// to the name of the first friend in the array
+const bestFriend = undefined
+
+// Don't change the code below this line
+module.exports = {
+  person,
+  mainInstrument,
+  bestFriend
+}
---
+++ js-fundamentals-objects/src/extension.js
@@ -0,0 +1,58 @@
+// This extension will require you to read the test failure messages to
+// figure out what you need to change, not all of the information has been
+// provided in the comments below.
+
+// Do not edit this book object directly
+const book = {
+  name: 'Clean Code',
+  author: 'Robert C. Martin',
+  category: 'Cooking',
+  isbn: {
+    isbn10: '9780132350884',
+    asin: '0132350882'
+  },
+  publisher: 'Prentice Hall',
+  dimensions: '10x12x2'
+}
+
+/* eslint-disable no-unused-vars */
+const isbn13 = '978-0132350884'
+
+// 1. Set this to the book name - using the book object
+const name = ''
+
+// 2. Set this to the isbn 10 value - using the book object
+const isbn10 = ''
+
+// Do not modify this basket object directly
+const basket = {
+  items: [
+    {
+      name: 'Apple',
+      quantity: 10,
+      price: 1
+    },
+    {
+      name: 'Lemon',
+      quantity: 2,
+      price: 0.5
+    }
+  ],
+  voucherCodes: ['AA-AA-A', 'BB-BB-B']
+}
+
+// 3. Set this variable to the length of the baskets voucher codes array - using the basket object
+const numberOfVoucherCodes = null
+
+// 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
+const firstVoucherCode = null
+
+// Do not edit this exported object
+module.exports = {
+  name: name,
+  isbn10: isbn10,
+  book: book,
+  basket: basket,
+  numberOfVoucherCodes: numberOfVoucherCodes,
+  firstVoucherCode: firstVoucherCode
+}
